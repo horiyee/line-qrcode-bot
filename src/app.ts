0456af210b927d070663560cd4ff8cebae6d7de5
horiyee <kaito@horiyee.dev>
2024-04-17 02:18:06 +0900

    feat: modify app.ts

--- src/app.ts
+++ src/app.ts
@@ -52,19 +52,17 @@ bot.on("message", async (event) => {
 
   await qrcode(eventMessageText)
     .then(async (base64Image) => {
-      await event.reply(`"${eventMessageText}" をQRコードに変換しました！`);
-
       const now = Date.now();
 
       const kv = await Deno.openKv();
       await kv.set(["qrcode", `${now}`], base64Image);
 
       const url = `${HOST}/images/${now}`;
-      await event.reply({
-        type: "image",
-        originalContentUrl: url,
-        previewImageUrl: url,
-      });
+
+      await event.reply([
+        `"${eventMessageText}" をQRコードに変換しました！`,
+        url,
+      ]);
     })
     .catch(console.error);
 });
